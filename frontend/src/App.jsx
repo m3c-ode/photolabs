9e9e9fd092438e335755f760e6e80ae0ff81d515
Maxime <maxime.marechalmccoy@gmail.com>
2023-10-04 14:28:18 -0700

    styled item v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,39 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+/**
+ * @typedef PhotoData
+ * @prop {string} id
+ * @prop {Object} location
+ * @prop {string} location.city
+ * @prop {string} location.country
+ * @prop {string} imageSource
+ * @prop {string} username
+ * @prop {string} profile
+ * 
+ */
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+
+  const photos = new Array(3).fill(<PhotoListItem photoData={sampleDataForPhotoListItem} />);
+  console.log("🚀 ~ file: App.jsx:32 ~ App ~ photos:", photos);
   return (
     <div className="App">
-      <PhotoListItem/>
+      {/* <PhotoListItem photoData={sampleDataForPhotoListItem} /> */}
+      {/* <PhotoListItem photoData={sampleDataForPhotoListItem} /> */}
+      {/* <PhotoListItem photoData={sampleDataForPhotoListItem} /> */}
+      {photos}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,43 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+/**
+ * @param {Object} props
+ * @param {PhotoData} props.photoData 
+ */
+const PhotoListItem = (props) => {
+  const photoData = props.photoData;
   /* Insert React */
+  return (
+    <div key={photoData.id} className="photo-list__item">
+      <img className="photo-list__image" src={photoData.imageSource} alt="img" />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={photoData.profile} alt="profile" />
+        <article className="photo-list__user-info">
+          <span>{photoData.username}</span>
+          <div className="photo-list__user-location">{photoData.location.city}, {photoData.location.country}</div>
+        </article>
+      </div>
+    </div>
+
+
+    // From the solution code
+    // <div className="photo-list__item">
+    //   {/* <PhotoFavButton toggleFavourite={props.toggleFavourite} photoId={props.photo.id} favourites={props.favourites} /> */}
+    //   <img /* onClick={() => props.setDisplayModal(props.photo)} */ className="photo-list__image" src={photoData.imageSource} />
+    //   <div className="photo-list__user-details ">
+    //     <img className="photo-list__user-profile" src={photoData.profile} />
+    //     <div className="photo-list__user-info">
+    //       <span>{photoData.username}</span>
+    //       <div className="photo-list__user-location">
+    //         {photoData.location.city},
+    //         {photoData.location.country}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+  );
 };
 
 export default PhotoListItem;
